0eba0be57f1884531577ffc7f2990f507bdbb8b8
oliverarias03 <oliverarias12@gmail.com>
2019-03-21 23:38:58 -0400

    tarea completa

---
+++ HTML_Helpers/HTML_Helpers/Controllers/FormController.cs
@@ -0,0 +1,42 @@
+﻿using HTML_Helpers.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HTML_Helpers.Controllers
+{
+    public class FormController : Controller
+    {
+        // GET: Form
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Formulario()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Formulario(Datos datos)
+        {
+            if (ModelState.IsValid)
+            {
+                return RedirectToAction("Resultado", datos);
+            }
+            else
+            {
+                return View("Formulario");
+            }
+        }
+
+        public ActionResult Resultado(Datos datos)
+        {
+            return View(datos);
+
+        }
+    }
+}
---
+++ HTML_Helpers/HTML_Helpers/Models/Datos.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace HTML_Helpers.Models
+{
+    public class Datos
+    {
+        [Required]
+        public string Cedula { get; set; }
+        [Required]
+        public string Nombre { get; set; }
+        [Required]
+        public string Apellido { get; set; }
+
+        [Range(15, 99, ErrorMessage = "DEBE SER MAYOR O IGUAL QUE 15")]
+        public int Edad { get; set; }
+
+        public string Telefono { get; set; }
+
+        [EmailAddress]
+        public string Correo { get; set; }
+
+        public Gender Genero { get; set; }
+
+        public enum Gender
+        {
+            Masculino,
+            Femenino
+        }
+
+        public string Estado { get; set; }
+
+        public bool Hobby1 { get; set; }
+
+        public bool Hobby2 { get; set; }
+
+        public bool Hobby3 { get; set; }
+
+        public bool Hobby4 { get; set; }
+
+
+
+    }
+}
